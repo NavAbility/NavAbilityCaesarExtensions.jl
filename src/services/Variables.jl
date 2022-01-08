6d742feb864bc7b800070d85c04fe5b042b1a745
Jim Hill <jim.hill.r@gmail.com>
2022-01-08 06:06:57 -0800

    Added getVariable and ls

--- src/services/Variables.jl
+++ src/services/Variables.jl
@@ -10,11 +10,21 @@ function addVariable!(dfg::NavAbilityDFG, variable::AbstractDFGVariable; maxWait
     sdkVariable = convert(dfg, variable)
     requestId = addPackedVariable(dfg.navabilityClient, context, sdkVariable)
     for _ in 1:maxWaitSeconds
-      savedVariable = getVariable(dfg.navabilityClient, context, variable.label)
+      savedVariable = NavAbilitySDK.getVariable(dfg.navabilityClient, context, variable.label)
       if !(savedVariable === nothing)
         break
       end
       sleep(1)
     end
     return requestId
+end
+
+function getVariable(dfg::NavAbilityDFG, label::Union{Symbol, String})
+  context = Client(dfg.userId,dfg.robotId,dfg.sessionId)
+  return NavAbilitySDK.getVariable(dfg.navAbilityClient, context, string(label))
+end
+
+function ls(dfg::NavAbilityDFG)
+  context = Client(dfg.userId,dfg.robotId,dfg.sessionId)
+  return NavAbilitySDK.ls(dfg.navAbilityClient, context)
 end
